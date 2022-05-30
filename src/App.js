e441a5c987d27b2b2b7a84a97a72433972657979
Lara <laraellenh@gmail.com>
2022-05-30 15:43:51 -0400

    added the tictactoe game to bottom of UI, dont want to spend the time making the paths/urls separate due to time constraints, next is another UI project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React, {useState, useEffect} from "react";
 import TodoContainer from './Components/TodoContainer'
 import NewToDo from './Components/NewToDo'
+import {Routes, Route, Link} from "react-router-dom"
+import TicTacToe from './Components/TicTacToe'
 
 function App() {
 const [tasks, setTasks] = useState([]);
@@ -30,12 +32,15 @@ useEffect(()=>{
       <div className="div"> Front End Dream Job Task List </div>
       <h4 className="h4"> Eyes on the prize </h4>
       <NewToDo onAddNewTask={addNewToDo} tasksArray= {taskList} setTasks={setTasks} />
+      <Link to= "/tictactoe"> Play TicTacToe </Link>
       <ul id="myUL">
       {taskList}
-      
+      <TicTacToe/>  
       </ul>
-    
-
+      {/* <Routes>
+        <Route path = "/tictactoe" element = {<TicTacToe />}>
+        </Route>
+      </Routes> */}
     </div>
     
   );
---
+++ src/Components/TicTacToe.js
@@ -0,0 +1,32 @@
+
+import { useState } from "react";
+// Build a react app that lets us play tic-tac-toe
+
+export default function TicTacToe() {
+    // initial state is an array of 9 elements
+  const [gameBoard, setGameBoard] = useState(Array(9).fill("-"));
+  const [player, setPlayer] = useState("X")
+  const handleClick = (index) => {
+      if(gameBoard[index] !== "-" ){
+          return ;
+      }
+      const newGameBoard = [...gameBoard];
+      newGameBoard[index] = player;
+      setGameBoard(newGameBoard);
+      setPlayer( player === "X" ? "O" : "X")
+  }
+  return (
+    <div >
+      <h1>Tic Tac Toe</h1>
+      {gameBoard.map((square, i) => {
+        return (
+          <span key={i}>
+            <button onClick={() => handleClick(i)}>{square}</button>
+            {i === 2 || i === 5 ? <br/> : ""}
+          </span>
+        );
+      })}
+    </div>
+
+  );
+}
